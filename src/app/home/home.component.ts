7108a2c4f42f2beae21886bbfe483e366517fedb
Ankit Gupta <ag7932613@gmail.com>
2020-10-27 21:25:36 +0530

    RxJs Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { PromotionService } from '../services/promotion.service';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -22,11 +23,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-      .then(dish => this.dish = dish);
+      .subscribe(dish => this.dish = dish);
     this.promotionservice.getFeaturedPromotion()
-      .then(promotion => this.promotion = promotion);
+      .subscribe(promotion => this.promotion = promotion);
     this.leaderservice.getFeaturedLeader()
-      .then(leader => this.leader = leader);
+      .subscribe(leader => this.leader = leader);
   }
 
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
 
+import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,23 +12,15 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS), 2000);
-    });
-  }
-  getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });
+  getLeader(id: number): Observable<Leader> {
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+  }
+
+  getFeaturedLeader(): Observable<Leader> {
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
   }
 }
