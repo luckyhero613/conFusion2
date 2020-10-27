043dd94f3f5a6835f7143b420ae7ae792293f832
Ankit Gupta <ag7932613@gmail.com>
2020-10-27 21:01:29 +0530

    Reactive promises part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -58,6 +59,7 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
