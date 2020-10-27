d68798d582a732530c7fabeec23334b26027440f
Ankit Gupta <ag7932613@gmail.com>
2020-10-27 19:37:50 +0530

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -50,6 +53,8 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     FlexLayoutModule,
     MatDialogModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
@@ -59,7 +64,8 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
     LoginComponent
